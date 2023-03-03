807c5bfa231749935ef59761cdb017afc57d520e
GiecoSofia <sofia.gieco@sancorsalud.com.ar>
2023-03-03 12:56:59 -0300

    iniciamos proyecto

---
+++ BootcampDH/src/main/java/com/SG_G1/BootcampDH/BootcampDhApplication.java
@@ -0,0 +1,13 @@
+package com.SG_G1.BootcampDH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampDhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampDhApplication.class, args);
+	}
+
+}
