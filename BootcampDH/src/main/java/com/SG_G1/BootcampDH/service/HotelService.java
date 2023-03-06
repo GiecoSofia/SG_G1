1594d62144e68f53e741cc510c819095150bad2b
PamelaAbeldanio <pbeldanio@gmail.com>
2023-03-06 13:19:04 -0300

    actualizando

--- BootcampDH/src/main/java/com/SG_G1/BootcampDH/service/HotelService.java
+++ BootcampDH/src/main/java/com/SG_G1/BootcampDH/service/HotelService.java
@@ -3,7 +3,6 @@ package com.SG_G1.BootcampDH.service;
 import com.SG_G1.BootcampDH.repository.HotelRepository;
 
 import java.util.List;
-
 public class HotelService {
     public List<HotelRepository> hotelRepositoryList;
 
