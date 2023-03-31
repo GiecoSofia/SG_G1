5959035f21b7355eee9e3cba886f261a9ed36975
GiecoSofia <sofia.gieco@sancorsalud.com.ar>
2023-03-31 12:35:11 -0300

    mapeo hotel model y flight model

--- BootcampDH/src/main/java/com/SG_G1/BootcampDH/model/FlightModel.java
+++ BootcampDH/src/main/java/com/SG_G1/BootcampDH/model/FlightModel.java
@@ -1,24 +1,44 @@
 package com.SG_G1.BootcampDH.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Date;
-@NoArgsConstructor
 @AllArgsConstructor
-@Data
+@NoArgsConstructor
+@Getter
+@Setter
 @Builder
+@Entity
+@Table(name= "flight")
 public class FlightModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "code")
     private String code;
+
+    @Column(name = "origin")
     private String origin;
+
+    @Column(name = "destination")
     private String destination;
+
+    @Column(name = "seatType")
     private String seatType;
+
+    @Column(name = "price", precision = 2)
     private Double price;
+
+    @Column(name = "date_from")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate from;
+
+    @Column(name = "date_to")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate to;
 
 
--- BootcampDH/src/main/java/com/SG_G1/BootcampDH/model/HotelModel.java
+++ BootcampDH/src/main/java/com/SG_G1/BootcampDH/model/HotelModel.java
@@ -1,26 +1,47 @@
 package com.SG_G1.BootcampDH.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.cglib.core.Local;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 @Builder
+@Entity
+@Table(name= "hotel")
 public class HotelModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "code")
     private String code;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "place")
     private String place;
+
+    @Column(name = "type")
     private String type;
+
+    @Column(name = "price", precision = 2)
     private Double price;
+
+    @Column(name= "date_from")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate from;
+
+    @Column(name= "date_to")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate to;
+
+    @Column(name= "booking")
     private Boolean booking;
 }
